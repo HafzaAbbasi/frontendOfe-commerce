93706a7bce0f74a6706e31edfcb0a0051247b9f5
Iftikhar23 <hafzaabbasi10@gmail.com>
2026-07-14 09:26:19 +0500

    updated

--- java.js
+++ java.js
@@ -1,13 +1,21 @@
-const header=document.querySelector("header");
+// 
 
-window.addEventListener("scroll",function(){
-    header.classList.toggle("sticky", this.window.scrollY>0);
-})
+const header = document.querySelector("header");
+const menu = document.querySelector('#menu-icon');
+const navmenu = document.querySelector('.navmenu');
 
-let menu=document.querySelector('#menu-icon');
-let navmenu=document.querySelector('.navmenu');
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-menu.onclick=()=>{
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open');
-}
+};
+
+// Close mobile menu when a nav link is clicked
+document.querySelectorAll('.navmenu a').forEach(link => {
+    link.addEventListener('click', () => {
+        navmenu.classList.remove('open');
+    });
+});
